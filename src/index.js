f8be5a80cc8938211feac9f671bc6c647672cc1b
unknown <lukacc008@gmail.com>
2022-04-09 15:11:16 +0200

    React router study

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { render } from "react-dom";
+import { 
+  BrowserRouter,
+  Routes,
+  Route,
+ } from "react-router-dom";
+import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoice from "./routes/invoice";
+import Invoices from "./routes/invoices";
+
+const rootElement = document.getElementById("root");
+render(
+<BrowserRouter>
+  <Routes>
+     <Route path="/" element={<App />}>
+     <Route path="expenses" element={<Expenses />} />
+     <Route path="invoices" element={<Invoices />}>
+      <Route path=":invoiceId" element={<Invoice />} />
+    </Route>
+     <Route
+      path="*"
+      element={
+        <main style={{ padding: "1rem" }}>
+          <p>There's nothing here!</p>
+        </main>
+      }
+    />
+     </Route>
+  </Routes>
+</BrowserRouter>,
+ rootElement
+ );
